65d4e2cfe31cbe17f20d43728dd44a3f352d204e
bbaster <bbaster@tutanota.com>
2025-04-07 00:32:42 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+
+'''
+Copyright (C) 2025 bbaster
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+import requests
+import re
+import string
+import random
+import sys
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from plyer import notification
+
+load_dotenv()
+userhash = os.getenv("USERHASH")
+
+def inform(url: str):
+    if re.search(r"^(https?://)?files.catbox.moe/\w{6}.\w+$", url):
+        print(url)
+        notification.notify(
+            title="Successfully uploaded to Catbox",
+            message=url,
+            timeout=10
+        )
+    else:
+        print("Upload to Catbox unsuccessful", file=sys.stderr)
+        notification.notify(
+            title="Upload to Catbox unsuccessful",
+            message="",
+            timeout=10
+        )
+        exit(1)
+    
+
+def upload(filepath: str) -> string:
+
+    filepath = Path(filepath)
+    
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:135.0) Gecko/20100101 Firefox/135.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-US,en',
+        'DNT': '1',
+        'Sec-GPC': '1',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1', 
+        'Priority': 'u=0, i',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+    }
+    
+    response_root = requests.get('https://catbox.moe/', headers=headers)
+    cookie = response_root.headers['Set-Cookie']
+    regex = re.search(r"^PHPSESSID=([0-9a-f]{32}); path=/$", cookie)
+    cookies = {'PHPSESSID': regex.group(1)}
+
+    form_boundary_id = ''
+    for _ in range(32):
+        form_boundary_id += random.choice(string.hexdigits[:16])
+    form_boundary = "------geckoformboundary" + form_boundary_id
+    
+    
+    headers.update({
+        'Accept': 'application/json',
+        'Referer': 'https://catbox.moe/',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Content-Type': f'multipart/form-data; boundary={form_boundary[2:]}',
+        'Origin': 'https://catbox.moe',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+    })
+
+    headers.pop('Sec-Fetch-User')
+    headers.pop('Priority')
+    
+    
+    with filepath.open('rb') as file:
+        contents = file.read()
+
+    filename = filepath.parts[-1]
+
+
+    data = f'''{form_boundary}
+Content-Disposition: form-data; name="reqtype"
+
+fileupload
+{form_boundary}
+Content-Disposition: form-data; name="userhash"
+
+{userhash}
+{form_boundary}
+Content-Disposition: form-data; name="fileToUpload"; filename="{filename}"
+Content-Type: application/octet-stream
+
+'''.encode("utf-8") + contents + f"\n{form_boundary}\n".encode("utf-8")
+
+    response_api = requests.post('https://catbox.moe/user/api.php', cookies=cookies, headers=headers, data=data)
+    return response_api.text
+
+
+if not sys.argv[1:]:
+    filepath = Path(input("Input a file path: "))
+    inform(upload(filepath))
+
+else:
+    for filepath in sys.argv[1:]:
+        inform(upload(filepath))
